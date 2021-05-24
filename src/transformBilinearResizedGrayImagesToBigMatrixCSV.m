5de0005c5632b96045d3a93e19d7b3ba01a7dec2
AndrazZrimsek <androzrim@gmail.com>
2021-05-24 23:26:24 +0200

    Added images

--- src/transformBilinearResizedGrayImagesToBigMatrixCSV.m
+++ src/transformBilinearResizedGrayImagesToBigMatrixCSV.m
@@ -12,11 +12,11 @@ function transformBilinearResizedGrayImagesToBigMatrixCSV()
     dataMatrix = [dataMatrix; reshaped_gray_image_with_label];
   end
 
-  filenames = ls('./../data/Slike Andra≈æ/Gray/Bilinear/');
-  labels = [0,0,0,0,0,0,0,0, 1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,2, 3,3,3,3,3,3,3,3, 4,4,4,4,4,4,4,4, 5,5,5,5,5,5,5,5, 6,6,6,6,6,6,6,6, 7,7,7,7,7,7,7,7, 8,8,8,8,8,8,8,8, 9,9,9,9,9,9,9,9];
+  filenames = ls('./../data/Slike Andraû/Gray/Bilinear/');
+  labels = [0,0,0,0,0,0,0,0,0,0,0,0, 1,1,1,1,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,2,2,2, 3,3,3,3,3,3,3,3,3,3,3,3, 4,4,4,4,4,4,4,4,4,4,4,4, 5,5,5,5,5,5,5,5,5,5,5,5, 6,6,6,6,6,6,6,6,6,6,6,6, 7,7,7,7,7,7,7,7,7,7,7,7, 8,8,8,8,8,8,8,8,8,8,8,8, 9,9,9,9,9,9,9,9,9,9,9,9];
             
   for i = 1:length(labels)
-    path_of_image = strcat('./../data/Slike Andra≈æ/Gray/Bilinear/', filenames(i, :));
+    path_of_image = strcat('./../data/Slike Andraû/Gray/Bilinear/', filenames(i, :));
     label = labels(i);
     gray_image = imread(path_of_image);
     reshaped_gray_image = reshape(gray_image', [1, 256]);
