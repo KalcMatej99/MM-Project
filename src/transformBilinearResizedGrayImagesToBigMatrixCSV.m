a5f9ad1215d6c117864738d3265257d0788a88c9
Matej <mk9693@student.uni-lj.si>
2021-05-25 18:19:43 +0200

    Added code for samo photos

--- src/transformBilinearResizedGrayImagesToBigMatrixCSV.m
+++ src/transformBilinearResizedGrayImagesToBigMatrixCSV.m
@@ -11,15 +11,28 @@ function transformBilinearResizedGrayImagesToBigMatrixCSV()
     end
   end
 
-  filenames = ls('./../data/Slike Andraû/Gray/Bilinear/');
+  filenames = ls('./../data/Slike Andra≈æ/Gray/Bilinear/');
   labels = [0,0,0,0,0,0,0,0,0,0,0,0, 1,1,1,1,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,2,2,2, 3,3,3,3,3,3,3,3,3,3,3,3, 4,4,4,4,4,4,4,4,4,4,4,4, 5,5,5,5,5,5,5,5,5,5,5,5, 6,6,6,6,6,6,6,6,6,6,6,6, 7,7,7,7,7,7,7,7,7,7,7,7, 8,8,8,8,8,8,8,8,8,8,8,8, 9,9,9,9,9,9,9,9,9,9,9,9];
             
   for i = 1:length(labels)
-    path_of_image = strcat('./../data/Slike Andraû/Gray/Bilinear/', filenames(i, :));
+    path_of_image = strcat('./../data/Slike Andra≈æ/Gray/Bilinear/', filenames(i, :));
     label = labels(i);
     gray_image = imread(path_of_image);
     reshaped_gray_image = reshape(gray_image', [1, 256]);
     reshaped_gray_image_with_label = [label, reshaped_gray_image];
     dataMatrix = [dataMatrix; reshaped_gray_image_with_label];
   end
+  
+  filenames = ls('./../data/Slike Samo/Gray/Bilinear/');
+  %labels = [0,0,0,0,0,0,0,0,0,0,0,0, 1,1,1,1,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,2,2,2, 3,3,3,3,3,3,3,3,3,3,3,3, 4,4,4,4,4,4,4,4,4,4,4,4, 5,5,5,5,5,5,5,5,5,5,5,5, 6,6,6,6,6,6,6,6,6,6,6,6, 7,7,7,7,7,7,7,7,7,7,7,7, 8,8,8,8,8,8,8,8,8,8,8,8, 9,9,9,9,9,9,9,9,9,9,9,9];
+            
+  for i = 1:100
+    path_of_image = strcat('./../data/Slike Samo/Gray/Bilinear/', filenames(i, :));
+    label = mod(i, 10);
+    gray_image = imread(path_of_image);
+    reshaped_gray_image = reshape(gray_image', [1, 256]);
+    reshaped_gray_image_with_label = [label, reshaped_gray_image];
+    dataMatrix = [dataMatrix; reshaped_gray_image_with_label];
+  end
+  
   dlmwrite('./../data/dataMatrixWithLabelsBilinear.csv', dataMatrix, ",");
--- src/transformRGBImagesToGrayImages.m
+++ src/transformRGBImagesToGrayImages.m
@@ -15,6 +15,7 @@ function transformRGBImagesToGrayImages(resizeMethod = 'bilinear')
     endfor
     
   endfor
+  
   filenames = ls(strcat('./../data/Slike Andraž/Color/'));
   number_of_images = rows(filenames);
   for i = 1:number_of_images
@@ -28,3 +29,21 @@ function transformRGBImagesToGrayImages(resizeMethod = 'bilinear')
     resized_gray_image = imresize(centered_gray_image, [16,16], resizeMethod);
     imwrite(resized_gray_image, strcat('./../data/Slike Andraž/Gray/', resizeMethod, '/', num2str(i), '.png'));
   endfor
+  
+  
+  filenames = ls(strcat('./../data/Slike Samo/Color/'));
+  number_of_images = rows(filenames);
+  for i = 1:number_of_images
+    path_of_image = strcat('./../data/Slike Samo/Color/', filenames(i, :));
+    if isrgb(imread(path_of_image))
+      gray_image = rgb2gray(imread(path_of_image));
+    else
+      gray_image = imread(path_of_image);
+    endif
+    centered_gray_image = center_gray_image(gray_image);
+    resized_gray_image = imresize(centered_gray_image, [16,16], resizeMethod);
+    label = mod(i,10);
+    imwrite(resized_gray_image, strcat('./../data/Slike Samo/Gray/', resizeMethod, '/', num2str(label), '.png'));
+  endfor
+  
+  
